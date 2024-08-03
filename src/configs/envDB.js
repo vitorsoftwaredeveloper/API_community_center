caaf3fd4cc3dc7a10c80ca727bdc0b1caa2c7c1b
vitorsoftwaredeveloper <vitorsoftwaredeveloper@gmail.com>
2024-08-02 21:54:09 -0300

    fix: change format object credentials

--- src/configs/envDB.js
+++ src/configs/envDB.js
@@ -1,4 +1,4 @@
 // - crie um novo arquivo igual este nesse mesmo diretório chamado env.js
 // - insira as credenciais de acesso, pois será esse arquivo que a configuração do banco de dados utilizará
 
-export const credentials = { password: ",", username: "" };
+export const credentials = { username: "", password: "" };
